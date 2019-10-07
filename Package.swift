6fbba346391d33327423154718038a56f05b5b0b
ramtinq <ramtindidab@gmail.com>
2019-10-07 11:06:27 +0330

    updated to be compatible with xCode 11 & swift 5

--- Package.swift
+++ Package.swift
@@ -1,23 +1,9 @@
-//
-//  Created by Jesse Squires
-//  http://www.jessesquires.com
-//
-//
-//  Documentation
-//  http://www.jessesquires.com/JSQWebViewController
-//
-//
-//  GitHub
-//  https://github.com/jessesquires/JSQWebViewController
-//
-//
-//  License
-//  Copyright © 2015 Jesse Squires
-//  Released under an MIT license: http://opensource.org/licenses/MIT
-//
-
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-    name: "JSQWebViewController"
+	name: "JSQWebViewController",
+	dependencies: [
+        //
+	]
 )
